99b00ed0df1b0c678fce8ca6e3b8422f9ea1503c
tawhidabdullah <tawhidabdullah22@gmail.com>
2019-03-25 16:51:58 +0600

    breaking Reacords

---
+++ Problem solving/breakingtheRecords.js
@@ -0,0 +1,28 @@
+
+function breakingRecords(scores) {
+    const maxAndWorstScores = [0,0]; 
+    let wrostScorePointer = 0; 
+    let maxScorePointer = 0;
+
+for(let maxScore = maxScorePointer+1; maxScore < scores.length; maxScore++){
+   if(scores[maxScorePointer] < scores[maxScore]) {
+       maxAndWorstScores[0] += 1; 
+       maxScorePointer++; 
+       maxScorePointer = maxScore; 
+   }
+}
+
+for(let worstScore = wrostScorePointer+1; worstScore < scores.length; worstScore++){
+     if(scores[wrostScorePointer] > scores[worstScore] ) {
+        maxAndWorstScores[1] +=1; 
+        wrostScorePointer = worstScore; 
+    
+    }
+ }
+
+  
+return maxAndWorstScores; 
+}
+
+
+console.log(breakingRecords([10, 5, 20, 20, 4, 5, 2, 25, 1])); // should be [2,4]
